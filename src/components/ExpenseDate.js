78b991bada21a6a3ba01763f5776b5a7a8cd6345
Robert Warren <warrenrobert127@gmail.com>
2022-03-13 21:20:38 -0500

    Expenses component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,4 @@
-
+import './ExpenseDate.css';
 function ExpenseDate(props) {
 
     const month = props.date.toLocaleString("en-US", { month: "long" });
@@ -8,10 +8,10 @@ function ExpenseDate(props) {
 
     return (
         
-      <div>
-          <div>{month}</div>
-          <div>{year}</div>
-          <div>{day}</div>
+      <div className= "expense-date">
+          <div className="expense-date__month">{month}</div>
+          <div className= "expense-date__year">{year}</div>
+          <div className= "expense-date__day">{day}</div>
       </div>
       
     );
