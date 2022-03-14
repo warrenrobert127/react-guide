800a1f555366c29239afc985d4bf4e0a3b538707
Robert Warren <warrenrobert127@gmail.com>
2022-03-13 22:49:18 -0500

    clean up components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,17 @@
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
-
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   
   return (
-    <div className="expense-item">
-        <ExpenseDate date={props.date}></ExpenseDate>
+    <Card className="expense-item">
+        <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseItem;
