2c88e43e81de9dcd24d8d95ecf71689228211d19
Robert Warren <warrenrobert127@gmail.com>
2022-04-05 20:30:12 -0500

    pass data up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-
+import React from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
-    { title: "Car Insurance", amount: 117.54, date: new Date(2021, 12, 15) },
-    { title: "Brake job", amount: 175.94, date: new Date(2020, 8, 29) },
-    { title: "Pizza", amount: 37.0, date: new Date(2021, 5, 10) },
-    { title: "Booze", amount: 90.54, date: new Date(2022, 3, 25) },
+    { id: 'e1', title: "Car Insurance", amount: 117.54, date: new Date(2021, 12, 15) },
+    { id: 'e2', title: "Brake job", amount: 175.94, date: new Date(2020, 8, 29) },
+    { id: 'e3', title: "Pizza", amount: 37.0, date: new Date(2021, 5, 10) },
+    { id: 'e4', title: "Booze", amount: 90.54, date: new Date(2022, 3, 25) },
   ];
+
+const addExpenseHandler = expenses => {
+  console.log('In App.js');
+  console.log(expenses);
+};
+
   return (
     <div>
-      <NewExpense>Let's get started!</NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}>Let's get started!</NewExpense>
       <Expenses items={expenses}></Expenses>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,11 +3,22 @@ import React from "react";
 import ExpenseForm from './ExpenseForm';              
 import './NewExpense.css';
 
-const NewExpense = () => {
- return <div className="new-expense">
-    <ExpenseForm />
+const NewExpense = (props) => {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+     const expenseData = {
+         ...enteredExpenseData,
+         id: Math.random().toString()                                                             
+     };
+     props.onAddExpense(expenseData);
+    };
+
+ return (
+   <div className="new-expense">
+     <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
      <form></form>
- </div>
+   </div>
+ );
 
 };
 
